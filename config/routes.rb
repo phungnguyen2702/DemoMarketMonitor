ffa8f42f8f1ec6296058b88c4c622bcbd11db992
PhungNV <phungnv@larion.com>
2018-04-11 15:07:23 +0700

    add chart name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#barchart'
 
-  get 'home/scartter'
+  get 'scartter', to: "home#scartter"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
