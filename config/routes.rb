fb54dcae934a9b5cd83a9fcbf71185524cffc2df
PhungNV <phungnv@larion.com>
2018-04-12 16:25:46 +0700

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root 'home#barchart'
+  get 'barchart',to: 'home#barchart'
 
   get 'scatter', to: "home#scatter"
 
-  get 'home', to: "home#index"
+  root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
